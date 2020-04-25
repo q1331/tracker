c9614c2d821afcfcef1ea66f7f0e6a2ce283b19a
ziq <zhou_ziq@163.com>
2020-04-25 11:52:45 +0800

    feat: 上传文件

--- src/main/java/com/wangshangyi/tracker/track/TrackController.java
+++ src/main/java/com/wangshangyi/tracker/track/TrackController.java
@@ -3,18 +3,27 @@ package com.wangshangyi.tracker.track;
 import com.wangshangyi.tracker.Traceno;
 import com.wangshangyi.tracker.TracenoExample;
 import com.wangshangyi.tracker.dao.TracenoMapper;
+import com.wangshangyi.tracker.util.CharUtil;
+import com.wangshangyi.tracker.util.LocalStorage;
 import com.wangshangyi.tracker.util.ResponseUtil;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
+import javax.crypto.spec.PSource;
+import java.io.IOException;
+import java.io.InputStream;
 
 @RestController
 public class TrackController {
 
     @Resource
     private TracenoMapper tracenoMapper;
+
+    LocalStorage localStorage = new LocalStorage();
 
     @RequestMapping("/track")
     public String track() {
@@ -31,12 +40,23 @@ public class TrackController {
         }else{
             TracenoExample example = new TracenoExample();
             example.or().andWidEqualTo(String.valueOf(wid));
-            long result = tracenoMapper.countByExample(example);
-            System.out.println("result  is  " + result);
             Traceno tcn = tracenoMapper.selectOneByExample(example);
             tcn.setStatus(status);
-//            Integer result = tracenoMapper.updateByPrimaryKey(tcn);
+            Integer result = tracenoMapper.updateByPrimaryKey(tcn);
             return ResponseUtil.ok(result);
+        }
+
+    }
+
+    @PostMapping("/upload")
+    public Object upload(@RequestParam("file") MultipartFile file) throws IOException {
+        String originalFilename = file.getOriginalFilename();
+        try{
+            localStorage.store(file.getInputStream(), file.getSize(), file.getContentType(), originalFilename);
+            return ResponseUtil.ok();
+        }catch (Exception e){
+            System.out.println(e);
+            return ResponseUtil.fail(400, e.toString());
         }
 
     }
